e36a9c56760b54c3e8e57c0215cb9bf1b938e469
Luis Colon <luiscolon0426@gmail.com>
2022-08-02 09:49:08 -0400

    Add files

---
+++ 0x00-lockboxes/0-lockboxes.py
@@ -0,0 +1,9 @@
+def canUnlockAll(boxes):
+    """ This method checks if all boxes in a list can be unlocked """
+    keyset = [0]
+    for key in keyset:
+        for content in boxes[key]:
+            if content not in keyset and content < len(boxes):
+                keyset.append(content)
+
+    return len(keyset) == len(boxes)
