b565d79cc4221337032e21788f553b69dc9ce829
Luis Colon <luiscolon0426@gmail.com>
2022-08-31 07:14:47 -0400

    betty friendly

--- 0x04-sandpiles/0-sandpiles.c
+++ 0x04-sandpiles/0-sandpiles.c
@@ -10,15 +10,15 @@
 
 void grid_addition(int grid1[3][3], int grid2[3][3])
 {
-    int i, j;
+	int i, j;
 
-    for (i = 0; i < 3; i++)
-    {
-        for (j = 0; j < 3; j++)
-        {
-            grid1[i][j] = grid1[i][j] + grid2[i][j];
-        }
-    }
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 3; j++)
+		{
+			grid1[i][j] = grid1[i][j] + grid2[i][j];
+		}
+	}
 }
 
 /**
@@ -29,20 +29,20 @@ void grid_addition(int grid1[3][3], int grid2[3][3])
 
 void grid_print(int grid[3][3])
 {
-    int i = 0;
-    int j = 0;
+	int i = 0;
+	int j = 0;
 
-    printf("=\n");
-    for (i = 0; i < 3; i++)
-    {
-        for (j = 0; j < 3; j++)
-        {
-            if (j)
-                printf(" ");
-            printf("%d", grid[i][j]);
-        }
-        printf("\n");
-    }
+	printf("=\n");
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 3; j++)
+		{
+			if (j)
+				printf(" ");
+			printf("%d", grid[i][j]);
+		}
+		printf("\n");
+	}
 }
 
 /**
@@ -54,18 +54,18 @@ void grid_print(int grid[3][3])
 
 int grid_is_ok(int grid[3][3])
 {
-    int i = 0;
-    int j = 0;
+	int i = 0;
+	int j = 0;
 
-    for (i = 0; i < 3; i++)
-    {
-        for (j = 0; j < 3; j++)
-        {
-            if (grid[i][j] > 3)
-                return (0);
-        }
-    }
-    return (1);
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 3; j++)
+		{
+			if (grid[i][j] > 3)
+				return (0);
+		}
+	}
+	return (1);
 }
 
 /**
@@ -76,35 +76,35 @@ int grid_is_ok(int grid[3][3])
 
 void grid_change(int grid1[3][3])
 {
-    int i = 0;
-    int j = 0;
-    int gridx[3][3];
+	int i = 0;
+	int j = 0;
+	int gridx[3][3];
 
-    for (i = 0; i < 3; i++)
-    {
-        for (j = 0; j < 3; j++)
-            gridx[i][j] = 0;
-    }
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 3; j++)
+			gridx[i][j] = 0;
+	}
 
-    for (i = 0; i < 3; i++)
-    {
-        for (j = 0; j < 3; j++)
-        {
-            if (grid1[i][j] > 3)
-            {
-                grid1[i][j] = grid1[i][j] - 4;
-                if ((i - 1) >= 0 && (i - 1) < 3)
-                    gridx[i - 1][j] += 1;
-                if ((j - 1) >= 0 && (j - 1) < 3)
-                    gridx[i][j - 1] += 1;
-                if ((i + 1) >= 0 && (i + 1) < 3)
-                    gridx[i + 1][j] += 1;
-                if ((j + 1) >= 0 && (j + 1) < 3)
-                    gridx[i][j + 1] += 1;
-            }
-        }
-    }
-    grid_addition(grid1, gridx);
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 3; j++)
+		{
+			if (grid1[i][j] > 3)
+			{
+				grid1[i][j] = grid1[i][j] - 4;
+				if ((i - 1) >= 0 && (i - 1) < 3)
+					gridx[i - 1][j] += 1;
+				if ((j - 1) >= 0 && (j - 1) < 3)
+					gridx[i][j - 1] += 1;
+				if ((i + 1) >= 0 && (i + 1) < 3)
+					gridx[i + 1][j] += 1;
+				if ((j + 1) >= 0 && (j + 1) < 3)
+					gridx[i][j + 1] += 1;
+			}
+		}
+	}
+	grid_addition(grid1, gridx);
 }
 
 /**
@@ -116,10 +116,10 @@ void grid_change(int grid1[3][3])
 
 void sandpiles_sum(int grid1[3][3], int grid2[3][3])
 {
-    grid_addition(grid1, grid2);
-    while (!grid_is_ok(grid1))
-    {
-        grid_print(grid1);
-        grid_change(grid1);
-    }
+	grid_addition(grid1, grid2);
+	while (!grid_is_ok(grid1))
+	{
+		grid_print(grid1);
+		grid_change(grid1);
+	}
 }
