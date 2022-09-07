edfacca302e0d760408d8650b5cd2e71e662aaf3
Luis Colon <luiscolon0426@gmail.com>
2022-09-07 07:26:25 -0400

    Add task

--- 0x05-linked_list_palindrome/0-is_palindrome.c
+++ 0x05-linked_list_palindrome/0-is_palindrome.c
@@ -11,23 +11,23 @@
 
 int is_palindrome(listint_t **head)
 {
-    listint_t *tmp = *head;
-    int array[2048], i = 0, j = 0, middle;
+	listint_t *tmp = *head;
+	int array[2048], i = 0, j = 0, middle;
 
-    if (head == NULL || (tmp != NULL && tmp->next == NULL))
-        return (1);
-    while (tmp != NULL)
-    {
-        array[i] = tmp->n;
-        tmp = tmp->next;
-        i++;
-    }
-    i--, middle = i / 2;
-    while (i >= middle && j <= middle)
-    {
-        if (array[j] != array[i])
-            return (0);
-        i--, j++;
-    }
-    return (1);
+	if (head == NULL || (tmp != NULL && tmp->next == NULL))
+		return (1);
+	while (tmp != NULL)
+	{
+		array[i] = tmp->n;
+		tmp = tmp->next;
+		i++;
+	}
+	i--, middle = i / 2;
+	while (i >= middle && j <= middle)
+	{
+		if (array[j] != array[i])
+			return (0);
+		i--, j++;
+	}
+	return (1);
 }
